8df43e010126c8807fe6b3f584c9e78dfc59de79
Kiki <Kiki@Kiki-PC>
2020-10-18 11:09:22 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan7/integer/PBOIF210119052Latihan7Integer.java
@@ -0,0 +1,33 @@
+package pboif2.pkg10119052.latihan7.integer;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * tipe data Integer
+ * 
+ */
+public class PBOIF210119052Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
